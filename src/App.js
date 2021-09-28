e68b1b0b0d991e7f5288f2b46b31f3fac1ad322c
Kshitiz <chaudhary.kshitiz123@gmail.com>
2021-09-28 12:43:51 +0545

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { ContextController } from "./context";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextController>
+      <Router>
+        <>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/tracks/:id" component={Lyrics} />
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </ContextController>
   );
 }
 
